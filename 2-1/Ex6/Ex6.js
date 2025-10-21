57799f6cf4f336ce53e8f153888fa3ac2a22270a
Joan Caballero <caballero.portoles.joan@gmail.com>
2025-10-21 12:19:14 +0200

    Ex5 sencer, bases Ex6

---
+++ 2-1/Ex6/Ex6.js
@@ -0,0 +1,9 @@
+// Exercici 1
+let noms = ['Anna', 'Bernat', 'Clara']
+
+// Exercici 3
+let numeros = [1, 2, 3, 4, 5, 6]
+let numerosParells = numeros.filter(num => num % 2 === 0)
+
+// Exercici 4
+let obj = {nom: 'Ona', edat: 25, ciutat: 'Barcelona'}
