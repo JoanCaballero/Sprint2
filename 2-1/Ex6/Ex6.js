32c67e68a03b43b0da8e530685228d8a1914cd82
Joan Caballero <caballero.portoles.joan@gmail.com>
2025-10-21 12:39:54 +0200

    Ex6 Complet

--- 2-1/Ex6/Ex6.js
+++ 2-1/Ex6/Ex6.js
@@ -1,9 +1,64 @@
 // Exercici 1
-let noms = ['Anna', 'Bernat', 'Clara']
+const noms = ['Anna', 'Bernat', 'Clara']
+const imprimirNoms = (noms) => {
+  noms.forEach(nom => console.log(nom));
+}
+
+// Exercici 2
+const imprimirNomsForOf = (noms) => {
+  for (let nom of noms) {
+    console.log(nom);
+  }
+}
 
 // Exercici 3
-let numeros = [1, 2, 3, 4, 5, 6]
+const numeros = [1, 2, 3, 4, 5, 6]
 let numerosParells = numeros.filter(num => num % 2 === 0)
 
 // Exercici 4
-let obj = {nom: 'Ona', edat: 25, ciutat: 'Barcelona'}
+const obj = {nom: 'Ona', edat: 25, ciutat: 'Barcelona'}
+const imprimirObj = (obj) => {
+    for (let key in obj) {
+        console.log(`${key}: ${obj[key]}`);
+    }
+}
+
+// Exercici 5
+const numsFinsA5 = (nums) => {
+    for (let num of nums) {
+        console.log(num);
+        if (num === 5) break;
+    }
+}
+
+// Exercici 6
+const imprimirNomsIPosicio = (noms) => {
+    let i = 0;
+    for (let nom of noms) {
+        console.log(`Posició ${i}: ${nom}`);
+        i++;
+    }
+}
+
+// HTML
+const addEventListenerClickToElement = (element, callback) => {
+  element.addEventListener('click', callback)
+}
+
+const exercici1Btn = document.getElementById('exercici1-btn')
+addEventListenerClickToElement(exercici1Btn, () => imprimirNoms(noms))
+
+const exercici2Btn = document.getElementById('exercici2-btn')
+addEventListenerClickToElement(exercici2Btn, () => imprimirNomsForOf(noms))
+
+const exercici3Btn = document.getElementById('exercici3-btn')
+addEventListenerClickToElement(exercici3Btn, () => console.log(numerosParells))
+
+const exercici4Btn = document.getElementById('exercici4-btn')
+addEventListenerClickToElement(exercici4Btn, () => imprimirObj(obj))
+
+const exercici5Btn = document.getElementById('exercici5-btn')
+addEventListenerClickToElement(exercici5Btn, () => numsFinsA5(numeros))
+
+const exercici6Btn = document.getElementById('exercici6-btn')
+addEventListenerClickToElement(exercici6Btn, () => imprimirNomsIPosicio(noms))
