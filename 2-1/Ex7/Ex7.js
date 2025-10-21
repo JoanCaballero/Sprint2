4822376e273b0a084ed913d0984392387073dece
Joan Caballero <caballero.portoles.joan@gmail.com>
2025-10-21 13:53:36 +0200

    Promises arreglades, ara només queda diseny i lògica de la web

--- 2-1/Ex7/Ex7.js
+++ 2-1/Ex7/Ex7.js
@@ -1,5 +1,5 @@
 // Exercici 1 i 2
-const promesaHolaMon = new Promise((resolve) => {
+const promesaHolaMonFuncio = () => new Promise((resolve) => {
     setTimeout(() => {
         resolve('Hola, món!');
     }, 2000);
@@ -8,23 +8,27 @@ const promesaHolaMon = new Promise((resolve) => {
 // Exercici 3
 const promesaHola =(missatge) => {
     return new Promise((resolve, reject) => {
+    setTimeout(() => {
         if (missatge === 'Hola') {
             resolve(missatge + ', món!');
         } else {
             reject('Missatge no reconegut');
         }
     }, 2000);
+    })
 } 
 
 // Exercici 4
 const mostraSalutacio = async () => {
-  const resultat = await promesaHolaMon;
-  console.log(resultat);
+    const promesaHolaMon = promesaHolaMonFuncio();
+    const resultat = await promesaHolaMon;
+    console.log(resultat);
 }
 
 // Exercici 5
 const imprimirSalutacioTryCatch = async () => {
     try {
+        const promesaHolaMon = promesaHolaMonFuncio();
         const missatge = await promesaHolaMon;
         console.log(missatge);
     } catch (error) {
@@ -33,13 +37,13 @@ const imprimirSalutacioTryCatch = async () => {
 }
 
 // Exercici 6
-const promesa1 = new Promise((resolve) => {
+const promesa1 = () => new Promise((resolve) => {
     setTimeout(() => {
         resolve('Primera promesa resolta');
     }, 2000);
 })
 
-const promesa2 = new Promise((resolve) => {
+const promesa2 = () => new Promise((resolve) => {
     setTimeout(() => {
         resolve('Segona promesa resolta');
     }, 3000);
@@ -52,11 +56,10 @@ const addEventListenerClickToElement = (element, callback) => {
 }
 
 const exercici12Btn = document.getElementById('exercici12-btn')
-addEventListenerClickToElement(exercici12Btn, () => promesaHolaMon.then(missatge => console.log(missatge)))
+addEventListenerClickToElement(exercici12Btn, () => promesaHolaMonFuncio().then(missatge => console.log(missatge)))
 
 const exercici3Btn = document.getElementById('exercici3-btn')
 addEventListenerClickToElement(exercici3Btn, () => {
-    promesaHola('Adeu').then(missatge => console.log(missatge));
     promesaHola('Hola').then(missatge => console.log(missatge));
 })
 
@@ -68,5 +71,5 @@ addEventListenerClickToElement(exercici5Btn, () => imprimirSalutacioTryCatch())
 
 const exercici6Btn = document.getElementById('exercici6-btn')
 addEventListenerClickToElement(exercici6Btn, () => {
-    Promise.all([promesa1, promesa2]).then((resultats) => console.log(resultats))
+    Promise.all([promesa1(), promesa2()]).then((resultats) => console.log(resultats))
 })
